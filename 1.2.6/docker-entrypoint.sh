af149635ef4b74d5e16311f4e510f1da7ea1bf36
aidar <aibariev@fix.ru>
2017-06-25 22:33:59 +0300

    Created docker image for hbase standalone 1.2.6

---
+++ 1.2.6/docker-entrypoint.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+set -e
+
+# Generate the config only if it doesn't exist
+if [ ! -f "conf/hbase-site.xml" ]; then
+    CONFIG="conf/hbase-site.xml"
+
+    echo "<?xml version=\"1.0\"?>" >> $CONFIG
+    echo "<?xml-stylesheet type=\"text/xsl\" href=\"configuration.xsl\"?>" >> $CONFIG
+    echo "<configuration>" >> $CONFIG
+    echo "<property><name>hbase.rootdir</name><value>file://$HBASE_DATA_DIR/hbase</value></property>" >> $CONFIG
+    echo "<property><name>hbase.zookeeper.property.dataDir</name><value>file://$HBASE_DATA_DIR/zookeeper</value></property>" >> $CONFIG
+    echo "</configuration>" >> $CONFIG
+fi
+
+exec "$@"
