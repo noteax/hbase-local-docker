63232364161dbc744625dcc222a1b2d91e54e088
aidar <aibariev@fix.ru>
2017-06-29 01:55:40 +0300

    Fixed region/master server hostnames

--- master/1.3.0/docker-entrypoint.sh
+++ master/1.3.0/docker-entrypoint.sh
@@ -12,9 +12,16 @@ if [ ! -f "conf/hbase-site.xml" ]; then
     echo "<property><name>hbase.rootdir</name><value>file://$HBASE_DATA_DIR</value></property>" >> $CONFIG
     echo "<property><name>hbase.cluster.distributed</name><value>true</value></property>" >> $CONFIG
     echo "<property><name>hbase.zookeeper.quorum</name><value>$ZOOKEEPER_QUORUM</value></property>" >> $CONFIG
+    echo "<property><name>hbase.master.hostname</name><value>$MASTER_HOSTNAME</value></property>" >> $CONFIG
     echo "<property><name>hbase.master.port</name><value>$MASTER_PORT</value></property>" >> $CONFIG
     echo "</configuration>" >> $CONFIG
 
+fi
+
+# Generate the regionservers only if it doesn't exist
+if [ ! -f "conf/regionservers" ]; then
+    CONFIG="conf/regionservers"
+    echo $REGIONSERVERS | tr , '\n' >> $CONFIG
 fi
 
 exec "$@"
--- regionserver/1.3.0/docker-entrypoint.sh
+++ regionserver/1.3.0/docker-entrypoint.sh
@@ -12,6 +12,7 @@ if [ ! -f "conf/hbase-site.xml" ]; then
     echo "<property><name>hbase.rootdir</name><value>file://$HBASE_DATA_DIR</value></property>" >> $CONFIG
     echo "<property><name>hbase.cluster.distributed</name><value>true</value></property>" >> $CONFIG
     echo "<property><name>hbase.zookeeper.quorum</name><value>$ZOOKEEPER_QUORUM</value></property>" >> $CONFIG
+    echo "<property><name>hbase.regionserver.hostname</name><value>$REGIONSERVER_HOSTNAME</value></property>" >> $CONFIG
     echo "<property><name>hbase.regionserver.port</name><value>$REGIONSERVER_PORT</value></property>" >> $CONFIG
     echo "</configuration>" >> $CONFIG
 
